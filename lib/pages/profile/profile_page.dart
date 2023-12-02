94e4c39bc321778bd286171921fdbd512e83bda0
Basliel-Amsalu <acychappiekiller@gmail.com>
2023-12-02 18:11:29 +0300

    profile

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ulearning/pages/profile/widgets/profile_widgets.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -12,7 +13,26 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: buildAppBar(),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              profileIconAndEditButton(),
+              SizedBox(
+                height: 30.h,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 25.w),
+                child: buildListView(),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
